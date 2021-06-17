31ad9ecd38138965df2c4c3102b4967e4380322b
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-06-16 23:53:53 -0300

    Adding mongo_utils

---
+++ src/SharedLibrary/mongo_utils.py
@@ -0,0 +1,84 @@
+import pymongo
+
+def connect_to_mongo(base_uri, user, password, database, port, host):
+    """ Connect to mongo using the Atlas uri
+    Args:
+        base_uri (str): uri to be filled with the user informations
+        user (str): user login
+        password (str): user password
+        database (str): database to connect
+        port (int): mongo cluster port
+        host (str): mongo host name
+    Returns:
+        MongoClient: the client to be used in the database/collection access
+    """
+    try:
+        connection_string = base_uri.format(
+            MONGO_USERNAME=user, MONGO_PASSWORD=password, MONGO_DB=database, MONGO_HOSTNAME=host)
+        return pymongo.MongoClient(connection_string, port)
+    except:
+        return None
+
+
+def get_mongo_database(connection, database_name):
+    """ Access the database
+    Args:
+        connection (MongoClient): Mongo connection to the database
+        database_name (str): database to be accessed
+    Returns:
+        Database: the Database object
+    """
+    try:
+        return connection.get_database(database_name)
+    except:
+        return None
+
+
+def get_mongo_collection(database, collection_name):
+    """ Access the collection
+    Args:
+        database (Database): Database that contains the collection_name
+        collection_name (str): collection to be accessed
+    Returns:
+        Collection: the Collection object
+    """
+    try:
+        return database.get_collection(collection_name)
+    except:
+        return None
+
+
+def delete_document_on_mongo(collection, query):
+    """ Deletes multiple document to the collection
+    Args:
+        collection (Collection): collection object to insert the document
+        query (dictionary): Query used to find the object and delete it in the collection
+    Returns:
+        bool: True if document was deleted correctly
+    """
+    print("[Debug] Deleting document on mongo")
+    try:
+        collection.delete_many(query)
+        print("[Debug] Document deleted")
+        return True
+    except:
+        print("[Warn] Couldn't delete the document")
+        return False
+
+
+def access_collection(parameters):
+    """ Access the mongo 
+    Args:
+        parameters (dictionary): dictionary object to with the env parameters
+    Returns:
+        tuple: the first element is a boolean (True if there's no error) and the second element is a collection object.
+    """
+    if (parameters is None):
+        return (False, None)
+
+    client = connect_to_mongo(parameters["connection_url"], parameters["username"],
+                              parameters["password"], parameters["database"], parameters["port"])
+    db = get_mongo_database(client, parameters["database"])
+    collection = get_mongo_collection(db, parameters["collection"])
+
+    return (collection is not None, collection)
