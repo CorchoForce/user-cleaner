74861435f933c7902f04dcb98ad931a750b35070
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-06-27 14:01:14 -0300

    Adding mongo connection

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,16 +1,23 @@
 import os
 from datetime import datetime
+from SharedLibrary.env_utils import load_parameters
+from SharedLibrary.mongo_utils import access_collection, delete_invalid_users
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
-scheduler = BlockingScheduler()
-hour = os.getenv('SCHEDULE_TIME_HOUR')
+SCHEDULER = BlockingScheduler()
+HOUR = os.getenv('SCHEDULE_TIME_HOUR')
+MONGO_PARAMETERS = load_parameters()
 
 
-@scheduler.scheduled_job(IntervalTrigger(hours=hour))
-def delete_invalid_users():
+@SCHEDULER.scheduled_job(IntervalTrigger(hours=HOUR))
+def users_cron():
+    failed, collection = access_collection(MONGO_PARAMETERS)
+    if failed:
+        raise Exception("Failed while trying to connect to mongo")
+    delete_invalid_users(collection)
     print('dask train_model! The time is: %s' % datetime.now())
 
 
-scheduler.start()
+SCHEDULER.start()
